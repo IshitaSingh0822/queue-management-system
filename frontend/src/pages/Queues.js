5cfdfd65697ba544d177ac3e1f36eb75a8e86479
IshitaSingh0822 <binita.ishita05@gmail.com>
2026-05-26 16:20:44 +0530

    fix unused Link import

--- frontend/src/pages/Queues.js
+++ frontend/src/pages/Queues.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { getQueues, createQueue, deleteQueue, updateQueue } from '../api';
 import { useSocket } from '../context/SocketContext';
 import { useToast } from '../context/ToastContext';
